5f65ddd595eb6953d6ca16c90ddf50ead03dbd35
vinicius5g <vinicius.victor.sm@gmail.com>
2021-03-17 06:53:06 -0300

    categories routes and category model

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import Category from '../model/Category';
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post('/', (req, res) => {
+  const { name, description } = req.body;
+
+  const category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
+
+  categories.push(category);
+
+  return res.status(201).json(category);
+});
+
+export { categoriesRoutes };
