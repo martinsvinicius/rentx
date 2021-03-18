c1170d615f566ba2b37c85ace04afa7702b74028
vinicius5g <vinicius.victor.sm@gmail.com>
2021-03-18 00:38:46 -0300

    using specificationRouter on server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
+import specificationsRouter from './routes/specifications.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/categories', categoriesRoutes);
+app.use('/specifications', specificationsRouter);
 
 app.listen(3333);
