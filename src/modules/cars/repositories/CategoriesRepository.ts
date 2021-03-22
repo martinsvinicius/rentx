08dd7c0af7fcb67b145a8556ad1cae00930e3365
vinicius5g <vinicius.victor.sm@gmail.com>
2021-03-22 16:33:47 -0300

    working with singleton pattern

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -4,9 +4,18 @@ import ICategoriesRepository from './ICategoriesRepository';
 
 class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[];
+    private static INSTANCE: CategoriesRepository;
 
-    constructor() {
+    private constructor() {
     	this.categories = [];
+    }
+
+    public static getInstance(): CategoriesRepository {
+      if (!CategoriesRepository.INSTANCE) {
+        CategoriesRepository.INSTANCE = new CategoriesRepository();
+      }
+
+      return CategoriesRepository.INSTANCE;
     }
 
     public list(): Category[] {
