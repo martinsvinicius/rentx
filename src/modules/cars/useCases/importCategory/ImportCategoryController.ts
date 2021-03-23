fe8dec9c892803962df55b75569f4a8c20c3a4b2
vinicius5g <vinicius.victor.sm@gmail.com>
2021-03-22 23:59:38 -0300

    saving categories from the csv import

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -4,12 +4,12 @@ import ImportCategoryUseCase from './ImportCategoryUseCase';
 class ImportCategoryController {
   constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
 
-  handle(req: Request, res: Response): Response {
+  async handle(req: Request, res: Response): Promise<Response> {
     const { file } = req;
 
-    const fileResponse = this.importCategoryUseCase.execute(file);
+    const categories = await this.importCategoryUseCase.execute(file);
 
-    return res.json(fileResponse);
+    return res.json(categories);
   }
 }
 
