59920b7f399509df3b353a4c9efbe88d679c7c91
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-09-30 15:18:33 -0300

    refactoring Category entity

---
+++ src/modules/cars/entities/Category.ts
@@ -0,0 +1,29 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('categories')
+class Category {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+
+    if (!this.created_at) {
+      this.created_at = new Date();
+    }
+  }
+}
+
+export default Category;
