002323e8404abcfff23bc710ae73ef992a03f64b
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-10-07 14:42:02 -0300

    refactoring CategoriesRepository

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -2,9 +2,9 @@ import ICreateCategoryDTO from '../dtos/ICreateCategoryDTO';
 import Category from '../entities/Category';
 
 interface ICategoriesRepository {
-  list(): Category[];
-  create({ name, description }: ICreateCategoryDTO): Category;
-  findByName(name: string): Category;
+  list(): Promise<Category[]>;
+  create({ name, description }: ICreateCategoryDTO): Promise<Category>;
+  findByName(name: string): Promise<Category>;
 }
 
 export default ICategoriesRepository;
--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,46 +1,47 @@
+import { getRepository, Repository } from 'typeorm';
 import ICreateCategoryDTO from '../../dtos/ICreateCategoryDTO';
 import Category from '../../entities/Category';
 import ICategoriesRepository from '../ICategoriesRepository';
 
 class CategoriesRepository implements ICategoriesRepository {
-    private categories: Category[];
-    private static INSTANCE: CategoriesRepository;
+  private repository: Repository<Category>;
+  private static INSTANCE: CategoriesRepository;
 
-    private constructor() {
-    	this.categories = [];
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
     }
 
-    public static getInstance(): CategoriesRepository {
-      if (!CategoriesRepository.INSTANCE) {
-        CategoriesRepository.INSTANCE = new CategoriesRepository();
-      }
+    return CategoriesRepository.INSTANCE;
+  }
 
-      return CategoriesRepository.INSTANCE;
-    }
+  private constructor() {
+    this.repository = getRepository(Category);
+  }
 
-    public list(): Category[] {
-      return this.categories;
-    }
+  public async list(): Promise<Category[]> {
+    return await this.repository.find();
+  }
 
-    public create({ name, description }: ICreateCategoryDTO): Category {
-    	const category = new Category();
+  public async create({
+    name,
+    description,
+  }: ICreateCategoryDTO): Promise<Category> {
+    const category = this.repository.create({
+      description,
+      name,
+    });
 
-    	Object.assign(category, {
-    		name,
-    		description,
-    	});
+    const createdCategory = await this.repository.save(category);
 
-    	this.categories.push(category);
+    return createdCategory;
+  }
 
-    	return category;
-    }
-
-    public findByName(name: string): Category {
-      const category = this.categories.find((category) =>
-        category.name === name);
-
-      return category;
-    }
+  public async findByName(name: string): Promise<Category> {
+    return await this.repository.findOne({
+      where: { name },
+    });
+  }
 }
 
 export default CategoriesRepository;
