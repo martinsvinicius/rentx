185f19172147c5f27bb082c7169ef27dc99cf88e
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-10-15 14:41:22 -0300

    adding missing down method implementation

--- src/database/migrations/1634316758671-CreateSpecifications.ts
+++ src/database/migrations/1634316758671-CreateSpecifications.ts
@@ -29,5 +29,7 @@ export class CreateSpecifications1634316758671 implements MigrationInterface {
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('specifications');
+  }
 }
