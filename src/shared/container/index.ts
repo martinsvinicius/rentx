b1a00c1557602a79da4dbd2a873c05675b774732
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-10-19 11:16:17 -0300

    adding users repository to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,6 @@
 import { container } from 'tsyringe';
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import ICategoriesRepository from '../../modules/cars/repositories/ICategoriesRepository';
 import CategoriesRepository from '../../modules/cars/repositories/implementations/CategoriesRepository';
 import SpecificationsRepository from '../../modules/cars/repositories/implementations/SpecificationsRepository';
@@ -14,4 +16,10 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   'SpecificationsRepository',
   SpecificationsRepository
+);
+
+// IUsersRepository
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository
 );
