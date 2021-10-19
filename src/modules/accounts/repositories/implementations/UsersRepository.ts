70e0cb0f3d700ec402ef8a163fbb556aa2431b85
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-10-19 11:15:39 -0300

    adding users repository implementation

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,26 @@
+import { getRepository, Repository } from 'typeorm';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async list(): Promise<User[]> {
+    return await this.repository.find();
+  }
+
+  async create(data: ICreateUserDTO): Promise<User> {
+    const user = this.repository.create({
+      ...data,
+    });
+
+    const createdUser = await this.repository.save(user);
+
+    return createdUser;
+  }
+}
