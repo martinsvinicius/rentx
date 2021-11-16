eaf6ec9eae601f850666cd58bfbff8b4d84a8b06
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-11-16 13:49:23 -0300

    add CarsRepository with typeorm

---
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -0,0 +1,34 @@
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { getRepository, Repository } from 'typeorm';
+import { Car } from '../entities/Car';
+
+export class CarsRepository implements ICarsRepository {
+  private repository: Repository<Car>;
+
+  constructor() {
+    this.repository = getRepository(Car);
+  }
+
+  async list(): Promise<Car[]> {
+    return await this.repository.find();
+  }
+
+  async create(car: ICreateCarDTO): Promise<Car> {
+    const newCar = this.repository.create(car);
+
+    return await this.repository.save(newCar);
+  }
+
+  async findById(id: string): Promise<Car> {
+    return await this.repository.findOne({
+      where: { id },
+    });
+  }
+
+  async findByLicensePlate(licensePlate: string): Promise<Car> {
+    return await this.repository.findOne({
+      where: { licensePlate },
+    });
+  }
+}
